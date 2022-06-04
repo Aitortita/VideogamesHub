30b6cf1091cf84f04f05649ac0fdc50c8e95bb9f
Aitor <98376581+Aitortita@users.noreply.github.com>
2022-06-03 23:50:59 -0300

    common fixes

--- Henry/PI-Videogames-main/client/src/components/Nav/Nav.jsx
+++ Henry/PI-Videogames-main/client/src/components/Nav/Nav.jsx
@@ -2,7 +2,7 @@ import styles from "./Nav.module.css";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as allActions from "../../redux/actions";
-import { useDispatch, useSelector} from "react-redux";
+import { useDispatch } from "react-redux";
 
 export default function Nav(props){
     const dispatch = useDispatch();
